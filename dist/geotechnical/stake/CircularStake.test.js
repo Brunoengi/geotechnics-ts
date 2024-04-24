a6f98aeae7dd4ec67481b75c48dc7e41ec68a6c3
Bruno <bruno.if@hotmail.com>
2024-04-24 07:19:13 -0300

    sucess in the first test

---
+++ dist/geotechnical/stake/CircularStake.test.js
@@ -0,0 +1,18 @@
+import { describe, expect, test } from '@jest/globals';
+import { CircularStake } from './CircularStake.js';
+describe('Test about Circular Stake', () => {
+    test('checking that all properties of the class are not null or not undefined', () => {
+        const stakeSection = new CircularStake({
+            diameter: 0.3,
+            height: 4,
+            inicialQuota: 1
+        });
+        // Verificar se todas as propriedades no objeto retornado não são null ou undefined
+        expect(stakeSection._height).not.toBeNull();
+        expect(stakeSection._height).not.toBeUndefined();
+        expect(stakeSection._area).not.toBeNull();
+        expect(stakeSection._area).not.toBeUndefined();
+        expect(stakeSection._perimeter).not.toBeNull();
+        expect(stakeSection._perimeter).not.toBeUndefined();
+    });
+});
